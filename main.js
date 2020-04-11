f58fb60d46b0048d8f8eaf1429e7e54919241f32
Slowcobo <spencer.coleman92@gmail.com>
2020-04-11 16:53:03 -0700

    Improved structure and animation

--- main.js
+++ main.js
@@ -1,21 +1,42 @@
-import { leftTitles, rightTitles } from "./titles.js";
+import { leftTitles, rightTitles } from './titles.js';
 
 let leftTitle = document.getElementById('left_title');
 let rightTitle = document.getElementById('right_title');
 let changeButton = document.getElementById('change');
 
-leftTitle.addEventListener('wheel', function(e) { e.preventDefault(); }, {passive: false} );
-rightTitle.addEventListener('wheel', function(e) { e.preventDefault(); }, {passive: false} );
+ //Clears currently active titles
+ const clearActive = () => {
+    let activeTitles = document.getElementsByClassName('active');
+    while (activeTitles.length > 0) {
+        activeTitles[0].classList.remove('active');
+    } 
+}
 
-leftTitles.forEach(element => {
-    leftTitle.innerHTML += '<div>' + element + '</div>';
-});
+//Picks a randomm title from each array
+const randomizeTitle = () => {
+    //Check for currently active titles
+    let currentTitles = document.getElementsByClassName('active');
+    let prevLeft = currentTitles[0];
+    let prevRight = currentTitles[1];
 
-rightTitles.forEach(element => {
-    rightTitle.innerHTML += '<div>' + element + '</div>';
-});
+    //Pick a random title from each array
+    let leftTitle = leftTitles[Math.floor(Math.random() * leftTitles.length)];
+    let rightTitle = rightTitles[Math.floor(Math.random() * rightTitles.length)];
+    
+    //If left or right title matches previously active title, randomize again
+    while (leftTitle === prevLeft) {
+        leftTitle = leftTitles[Math.floor(Math.random() * leftTitles.length)];
+    }
+    while (rightTitle === prevRight) {
+        rightTitle = rightTitles[Math.floor(Math.random() * rightTitles.length)];
+    }
 
+    return [leftTitle, rightTitle];
+}
+
+//Sets titles as active on the front-end
 const setActive = (currLeft, currRight) => {
+    //Find the titles in the list and set them active
     for (let i = 0; i < leftTitle.children.length; i++) {
         if (leftTitle.children[i].textContent === currLeft) {
             leftTitle.children[i].className = 'active';
@@ -29,40 +50,63 @@ const setActive = (currLeft, currRight) => {
     }
  }
 
- const clearActive = () => {
-    let actives = document.getElementsByClassName("active");
-    while (actives.length > 0) {
-        actives[0].classList.remove('active');
-    } 
- }
-
- const centerActive = () => {
+//Scrolls active titles to the center
+const centerActive = () => {
     let leftActive = leftTitle.getElementsByClassName('active')[0];
     let rightActive = rightTitle.getElementsByClassName('active')[0];  
     let isScrolling;
     
-    leftTitle.addEventListener('scroll', function (event) {
-		window.clearTimeout(isScrolling);
-		isScrolling = setTimeout(function() {
-            rightActive.scrollIntoView({
-                behavior: "smooth",
-                block: "center"
-            });
-		}, 66);
-	}, false);
+leftTitle.addEventListener('scroll', function (event) {
+    window.clearTimeout(isScrolling);
+    isScrolling = setTimeout(function() {
+        rightActive.scrollIntoView({
+            behavior: 'smooth',
+            block: 'center'
+        });
+    }, 66);
+}, false);
 
-    leftActive.scrollIntoView({
-        behavior: "smooth",
-        block: "center"
+leftActive.scrollIntoView({
+    behavior: 'smooth',
+    block: 'center'
     });
- }
+}
 
- changeButton.onclick = () => {
-     clearActive();
-     
-     let currLeft = leftTitles[Math.floor(Math.random() * leftTitles.length)];  
-     let currRight = rightTitles[Math.floor(Math.random() * rightTitles.length)];
-     
-     setActive(currLeft, currRight);
-     centerActive();
- }
+//Prevent manual scrolling through titles
+leftTitle.addEventListener('wheel', function(e) { e.preventDefault(); }, {passive: false} );
+rightTitle.addEventListener('wheel', function(e) { e.preventDefault(); }, {passive: false} );
+
+//Prepend lists with filler blanks
+leftTitle.innerHTML += '<div class="filler"></div>';
+rightTitle.innerHTML += '<div class="filler"></div>';
+
+//Populate lists with titles
+leftTitles.forEach(element => {
+    leftTitle.innerHTML += '<div>' + element + '</div>';
+});
+
+rightTitles.forEach(element => {
+    rightTitle.innerHTML += '<div>' + element + '</div>';
+});
+
+//Append lists with filler blanks
+leftTitle.innerHTML += '<div class="filler"></div>';
+rightTitle.innerHTML += '<div class="filler"></div>';
+
+//Set initial active to first elemenets of each title list
+setActive(leftTitles[0], rightTitles[0]);
+
+//Clicking button will randomize title
+changeButton.onclick = () => {
+    //Clear the current active title
+    clearActive();
+    
+    //Grab new random titles
+    let newTitles = randomizeTitle();
+
+    //Set the random titles as active
+    setActive(newTitles[0], newTitles[1]);
+
+    //Scroll active titles to the center
+    centerActive();
+}
